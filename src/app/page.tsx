328ef7ac98d75636416450ab1ba894484057f10f
JaySant <jaysant15@gmail.com>
2024-06-08 15:56:54 -0300

    feat: Add interface para tipagem e a pagina inicial do portal de noticias.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,31 @@
-export default function Home() {
-    return <h1>Pagina inicial</h1>
+"use client";
+
+import axios from "axios";
+import { useEffect, useState } from "react"
+import { Article } from "./type/Article";
+export default function inicialPage() {
+    const [articles, setArticles ] = useState<Article[]>([])
+
+    useEffect(() => {
+        axios.get('api/route').then((response) => {
+            setArticles(response.data.articles)
+        })
+    }, [])
+
+        const filterArticles = articles.filter(articles => articles.title
+        && articles.urlToImage).slice(0, 7)
+
+    return (
+        <div className="container">
+            <h1>Noticias do Dia</h1>
+            <div className="card-top"></div>
+            {filterArticles.map((articles, index) => (
+                <div key={index} className="card">
+                    <img src={articles.urlToImage}></img>
+                    <h1>{articles.title}</h1>
+                    <p>{articles.description}</p>
+                </div>
+            ))}     
+        </div>
+    )
 }
