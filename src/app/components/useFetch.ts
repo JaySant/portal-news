a430475c2eb4d650fc21c2d819d3bc42b6c91447
JaySant <jaysant15@gmail.com>
2024-06-09 13:51:19 -0300

    feat: Add um arquivo hook customizado que busca os dados da API,tambem foi feito ajuste na rota da API

---
+++ src/app/components/useFetch.ts
@@ -0,0 +1,20 @@
+'use client'; 
+
+import axios from "axios"
+import { useEffect, useState } from "react"
+import { Article } from "../type/Article"
+
+export const useFetch = () => {
+    const [articles, setArticles ] = useState<Article[]>([])
+    const [loading, setLoading] = useState<boolean>(true);
+
+    useEffect(() => {
+        axios.get(`${window.location.origin}/api/route`).then((response) => {
+            setArticles(response.data.articles)
+            setLoading(false)
+        })
+
+    }, []);
+
+    return {articles, loading};
+}
